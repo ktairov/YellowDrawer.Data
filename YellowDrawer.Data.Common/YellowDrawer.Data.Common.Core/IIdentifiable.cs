5f7df25952986cb9b3651375b9851e8ef90daf91
Kirill Tairov <pyro.sun@gmail.com>
2017-04-17 18:07:45 +0300

    Add core .Net

---
+++ YellowDrawer.Data.Common/YellowDrawer.Data.Common.Core/IIdentifiable.cs
@@ -0,0 +1,12 @@
+﻿namespace YellowDrawer.Data.Common
+{
+    public interface IIdentifiable<out T> : IIdentifiable
+    {
+        new T Id { get; }
+    }
+
+    public interface IIdentifiable
+    {
+        object Id { get; }
+    }
+}
---
+++ YellowDrawer.Data.Common/YellowDrawer.Data.Common.Core/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace YellowDrawer.Data.Common
+{
+    public interface IRepository
+    {
+        void Add<T>(T item) where T : class, IIdentifiable;
+        void Update<T>(T item) where T : class, IIdentifiable;
+        void DeleteItem<T>(T item) where T : class, IIdentifiable;
+        void Delete<T>(object id) where T : class, IIdentifiable;
+        T Find<T>(object id) where T : class, IIdentifiable;
+        IQueryable<T> Find<T>(Expression<Func<T, bool>> predicate) where T : class, IIdentifiable;
+    }
+}
